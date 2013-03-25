ba8c9c4a499dd24cdfcac8d3f88264a9aa91baba
Oleg Hahm <oleg@hobbykeller.org>
2013-03-25 14:26:23 +0100

    * corrected HSE value for original discovery board oscillator

--- cortex-m/CMSIS/Device/ST/STM32F4xx/include/stm32f4xx.h
+++ cortex-m/CMSIS/Device/ST/STM32F4xx/include/stm32f4xx.h
@@ -12,15 +12,15 @@
   *          is using in the C source code, usually in main.c. This file contains:
   *           - Configuration section that allows to select:
   *              - The device used in the target application
-  *              - To use or not the peripheral’s drivers in application code(i.e. 
-  *                code will be based on direct access to peripheral’s registers 
+  *              - To use or not the peripheral?s drivers in application code(i.e. 
+  *                code will be based on direct access to peripheral?s registers 
   *                rather than drivers API), this option is controlled by 
   *                "#define USE_STDPERIPH_DRIVER"
   *              - To change few application-specific parameters such as the HSE 
   *                crystal frequency
   *           - Data structures and the address mapping for all peripherals
   *           - Peripheral's registers declarations and bits definition
-  *           - Macros to access peripheral’s registers hardware
+  *           - Macros to access peripheral?s registers hardware
   *  
   ******************************************************************************
   * @attention
@@ -95,7 +95,7 @@
   */           
 
 #if !defined  (HSE_VALUE) 
-  #define HSE_VALUE    ((uint32_t)16000000) /*!< Value of the External oscillator in Hz */
+  #define HSE_VALUE    ((uint32_t)8000000) /*!< Value of the External oscillator in Hz */
 #endif /* HSE_VALUE */
 
 /**

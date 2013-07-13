e5c27364f9934d821a18d981c0d4a500e5168540
southernbear <xq5530303@gmail.com>
2013-07-14 02:59:23 +0800

    Cleanup: cpu.c

--- stm32f407vgt6/cpu.c
+++ stm32f407vgt6/cpu.c
@@ -6,44 +6,13 @@
  */
 
 #include "stdint.h"
+
 #include "stm32f4xx.h"
 
 #include "cpu.h"
 #include "sched.h"
 
 extern void sched_task_exit(void);
-void sched_task_return(void);
-
-void stm32f4_pclk_scale(uint32_t source, uint32_t target, uint32_t* pclksel, uint32_t* prescale)
-{
-	uint32_t pclkdiv;
-	*prescale = source / target;
-
-	if( (*prescale % 4) == 0 ) {
-		*pclksel = TIM_CKD_DIV4;
-		pclkdiv = 4;
-	} else if( (*prescale % 2) == 0 ) {
-		*pclksel = TIM_CKD_DIV2;
-		pclkdiv = 2;
-	} else {
-		*pclksel = TIM_CKD_DIV1;
-		pclkdiv = 1;
-	}
-	*prescale /= pclkdiv;
-
-	if( *prescale % 2 )
-		(*prescale)++;
-}
-
-void cpu_clock_scale(uint32_t source, uint32_t target, uint32_t* prescale) {
-    uint32_t pclksel;
-
-    stm32f4_pclk_scale(source, target, &pclksel, prescale);
-
-    //PCLKSEL0 = (PCLKSEL0 & ~(BIT2|BIT3)) | (pclksel << 2); 		// timer 0
-    //PCLKSEL0 = (PCLKSEL0 & ~(BIT4|BIT5)) | (pclksel << 4); 		// timer 1
-    //PCLKSEL1 = (PCLKSEL1 & ~(BIT12|BIT13)) | (pclksel << 12);	// timer 2
-}
 
 __attribute__( ( always_inline ) ) static __INLINE void save_context(void)
 {
@@ -100,45 +69,6 @@ void PendSV_Handler(void)
     save_context();
     sched_run();
     restore_context();
-}
-
- /* kernel functions */
-void ctx_switch(void)
-{
-	/* Save return address on stack */
-	/* stmfd   sp!, {lr} */
-
-	/* disable interrupts */
-	/* mov     lr, #NOINT|SVCMODE */
-	/* msr     CPSR_c, lr */
-	/* cpsid 	i */
-
-	/* save other register */
-	asm("nop");
-
-	asm("mov r12, sp");
-	asm("stmfd r12!, {r4-r11}");
-
-	/* save user mode stack pointer in *active_thread */
-	asm("ldr     r1, =active_thread"); /* r1 = &active_thread */
-	asm("ldr     r1, [r1]"); /* r1 = *r1 = active_thread */
-	asm("str     r12, [r1]"); /* store stack pointer in tasks pdc*/
-
-	sched_task_return();
-}
-
-/* call scheduler so active_thread points to the next task */
-void sched_task_return(void)
-{
-	/* load pdc->stackpointer in r0 */
-	asm("ldr     r0, =active_thread"); /* r0 = &active_thread */
-	asm("ldr     r0, [r0]"); /* r0 = *r0 = active_thread */
-	asm("ldr     sp, [r0]"); /* sp = r0  restore stack pointer*/
-	asm("pop		{r4}"); /* skip exception return */
-	asm(" pop		{r4-r11}");
-	asm(" pop		{r0-r3,r12,lr}"); /* simulate register restor from stack */
-//	asm("pop 		{r4}"); /*foo*/
-	asm("pop		{pc}");
 }
 
 /*

00bc69188d7c6e21802602161ae44b7be285d8a7
Oleg Hahm <oleg@hobbykeller.org>
2014-02-19 16:02:06 +0100

    added missing defines for stm32f407vgt6 cpu-conf

--- stm32f407vgt6/include/cpu-conf.h
+++ stm32f407vgt6/include/cpu-conf.h
@@ -67,8 +67,10 @@ and the mailinglist (subscription via web site)
  * @name Kernel configuration
  * @{
  */
+#define KERNEL_CONF_STACKSIZE_PRINTF    (4096)
+
 #ifndef KERNEL_CONF_STACKSIZE_DEFAULT
-#define KERNEL_CONF_STACKSIZE_DEFAULT	2500
+#define KERNEL_CONF_STACKSIZE_DEFAULT	(512)
 #endif
 
 #define KERNEL_CONF_STACKSIZE_IDLE		500

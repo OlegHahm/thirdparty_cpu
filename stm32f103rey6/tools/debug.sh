6fcd2566efb19ea202b8f00b113c7f5db053448c
Oleg Hahm <oleg@hobbykeller.org>
2014-03-06 13:51:46 +0000

    use BOARD variable in debug script

--- stm32f103rey6/tools/debug.sh
+++ stm32f103rey6/tools/debug.sh
@@ -8,7 +8,7 @@ fi
 
 BIN_FOLDER=$(dirname "${FILE}")
 
-openocd -f "${BIN_FOLDER}/iot-lab_m3_jtag.cfg" \
+openocd -f "${BIN_FOLDER}/${BOARD}_jtag.cfg" \
 	-f "target/stm32f1x.cfg" \
   -c "tcl_port 6333"
   -c "telnet_port 4444"

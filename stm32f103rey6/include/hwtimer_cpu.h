a4abc45f85eb7a5d47e0a6523b89c5a21c28aa9b
Thomas Eichinger <thomas.eichinger1@gmail.com>
2014-06-24 12:26:22 +0200

    set HWTIMER_SPEED correctly

--- stm32f103rey6/include/hwtimer_cpu.h
+++ stm32f103rey6/include/hwtimer_cpu.h
@@ -2,7 +2,7 @@
 #define HWTIMER_CPU_H_
 
 #define HWTIMER_MAXTIMERS   (4)
-#define HWTIMER_SPEED       (2303U)
+#define HWTIMER_SPEED       (2000U)
 #define HWTIMER_MAXTICKS    (0xFFFF)
 
 #endif /* HWTIMER_CPU_H_ */
